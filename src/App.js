45c2ca9507267a83b6bdec2d27ee2f27014c8dc5
George <moraitis.georgios@icloud.com>
2022-10-01 20:43:03 +0300

    Added validation logic

--- src/App.js
+++ src/App.js
@@ -1,117 +1,195 @@
 import React from 'react';
 import './App.css';
 
-// Storing the initial state in a constant variable outside of the component
-const defaultState = {
-  firstName: null,
-  lastName: null,
-  email: null,
-  players: null,
-  date: null,
-  time: null,
+// Email validation
+const validEmailRegex = RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+);
+// Name validation
+const validNameRegex = RegExp(/^[a-z ,.'-]+$/i);
+
+// Form validation
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach(function (errorMessage) {
+    if (errorMessage.length > 0) {
+      valid = false;
+    }
+  });
+  return valid;
 };
 
+// Storing the initial state in a constant variable 
+const defaultState = {
+  submitting: false,
+  data: {
+    firstName: null,
+    lastName: null,
+    email: null,
+    players: null,
+    date: null,
+    time: null,
+  },
+  errors: {
+    firstName: '',
+    lastName: '',
+    email: '',
+  },
+};
 
-export default class App extends React.Component {
+export default class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = defaultState;
   }
 
-
   handleChange = (event) => {
-    console.log(event.target.value);
-  }
+    event.preventDefault();
+    const { name, value } = event.target;
+    const { data } = this.state;
+    // we assign the value of the state to a variable errors so we can use it in the switch statement
+    let errors = this.state.errors;
 
+    switch (name) {
+      case 'firstName':
+        errors.firstName =
+          value.length > 3 && validNameRegex.test(value)
+            ? ''
+            : 'First name is not valid! Must be at least 3 letters long!';
+        break;
+      case 'lastName':
+        errors.lastName =
+          value.length > 3 && validNameRegex.test(value)
+            ? ''
+            : 'Last name is not valid! Must be at least 3 letters long!';
+        break;
+      case 'email':
+        errors.email = validEmailRegex.test(value) ? '' : 'Email is not valid!';
+        break;
+      default:
+        break;
+    }
 
+    // We put inside the state the errors and the data, while we take a copy of the data
+    // using the spread operator and we assign in that copy every change that we make in the state
+    this.setState({ errors, data: { ...data, [name]: value } });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (validateForm(this.state.errors)) {
+      console.info('Valid Form');
+    } else {
+      console.error('Invalid Form');
+    }
+  };
 
   render() {
+    // Passing state to the render method
+    const { errors } = this.state;
     return (
-      <div className='wrapper'>
-        <div className='form-wrapper'>
+      <div>
+        <div>
           <h2>LazerTag World 🔫</h2>
-          <form>
-            {/* First Name */}
-            <div>
-              <label>First Name</label>
-              <input
-                type="text"
-                name="firstName"
-                onChange={this.handleChange}
-                required
-              />
-            </div>
+            <form onSubmit={this.handleSubmit}>
+              {/* First Name */}
+              <div>
+                <label>First Name</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  onChange={this.handleChange}
+                  required
+                />
+                {errors.firstName.length > 0 && (
+                  <span className="error">{errors.firstName}</span>
+                )}
+              </div>
 
-            {/* Last Name */}
-            <div>
-              <label>Last Name</label>
-              <input
-                type="text"
-                name="lastName"
-                onChange={this.handleChange}
-                required
-              />
-            </div>
-            {/* Email */}
-            <div>
-              <label>Email</label>
-              <input
-                type="email"
-                name="email"
-                onChange={this.handleChange}
-                required
-              />
-            </div>
-            {/* Players */}
-            <div>
-              <label>Players</label>
-              <input
-                type="number"
-                min="0"
-                max="25"
-                name="players"
-                onChange={this.handleChange}
-                required
-              />
-            </div>
-            {/* Date*/}
-            <div className="date">
-              <label>Choose a date :</label>
-              <input
-                type="date"
-                name="date"
-                onChange={this.handleChange}
-                min="2022-1-01"
-                max="2023-12-31"
-                required
-              />
-            </div>
-            <div className="time">
-              <label>Choose an hourly session : </label>
-              <select name="time" onChange={this.handleChange} required>
-                <option disabled selected value>
-                  -- Please select an option --
-                </option>
-                <option value="08:00AM - 09:00AM">08:00AM - 09:00AM</option>
-                <option value="09:00AM - 10:00AM">09:00AM - 10:00AM</option>
-                <option value="10:00AM - 11:00A">10:00AM - 11:00AM</option>
-                <option value="11:00AM - 12:00PM">11:00AM - 12:00PM</option>
-                <option value="12:00PM - 1:00PM">12:00PM - 1:00PM</option>
-                <option value="1:00PM - 2:00PM">1:00PM - 2:00PM</option>
-                <option value="2:00PM - 3:00PM">2:00PM - 3:00PM</option>
-                <option value="3:00PM - 4:00PM">3:00PM - 4:00PM</option>
-                <option value="4:00PM - 5:00PM">4:00PM - 5:00PM</option>
-                <option value="5:00PM - 6:00PM">5:00PM - 6:00PM</option>
-                <option value="6:00PM - 7:00PM">6:00PM - 7:00PM</option>
-              </select>
-            </div>
-            <div className='submit'>
-              <button>Create</button>
-            </div>
-          </form>
+              {/* Last Name */}
+              <div>
+                <label>Last Name</label>
+                <input
+                  type="text"
+                  name="lastName"
+                  onChange={this.handleChange}
+                  required
+                />
+                {errors.lastName.length > 0 && (
+                  <span className="error">{errors.lastName}</span>
+                )}
+              </div>
+              {/* Email */}
+              <div>
+                <label>Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={this.handleChange}
+                  required
+                />
+                {errors.email.length > 0 && (
+                  <span className="error">{errors.email}</span>
+                )}
+              </div>
+
+              {/* Players */}
+              <div>
+                <label>Players</label>
+                <input
+                  type="number"
+                  min="0"
+                  max="25"
+                  name="players"
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+
+              {/* Date*/}
+              <div>
+                <label>Choose a date :</label>
+                <input
+                  type="date"
+                  name="date"
+                  onChange={this.handleChange}
+                  min="2022-1-01"
+                  max="2023-12-31"
+                  required
+                />
+              </div>
+
+              {/* Time */}
+              <div className="time">
+                <label>Choose an hourly session : </label>
+                <select name="time" onChange={this.handleChange} required>
+                  <option disabled selected value>
+                    -- Please select an option --
+                  </option>
+                  <option value="08:00AM - 09:00AM">08:00AM - 09:00AM</option>
+                  <option value="09:00AM - 10:00AM">09:00AM - 10:00AM</option>
+                  <option value="10:00AM - 11:00A">10:00AM - 11:00AM</option>
+                  <option value="11:00AM - 12:00PM">11:00AM - 12:00PM</option>
+                  <option value="12:00PM - 1:00PM">12:00PM - 1:00PM</option>
+                  <option value="1:00PM - 2:00PM">1:00PM - 2:00PM</option>
+                  <option value="2:00PM - 3:00PM">2:00PM - 3:00PM</option>
+                  <option value="3:00PM - 4:00PM">3:00PM - 4:00PM</option>
+                  <option value="4:00PM - 5:00PM">4:00PM - 5:00PM</option>
+                  <option value="5:00PM - 6:00PM">5:00PM - 6:00PM</option>
+                  <option value="6:00PM - 7:00PM">6:00PM - 7:00PM</option>
+                </select>
+
+              </div>
+
+              <div className="submit">
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+        </div>
+
+        <div>
         </div>
       </div>
     );
   }
 }
-
